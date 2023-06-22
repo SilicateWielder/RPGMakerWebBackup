d6c0c88ded3dc75fc0313caae709b3973751a242
M W <michaelwarner@mediadeck.tv>
2023-06-22 02:25:06 -0500

    Update backup.js
    Implemented foundation for overhaul that relies more on StorageManager.js within RPGMaker MV games.

--- js/backup.js
+++ js/backup.js
@@ -1,11 +1,98 @@
+////////////////////////////////
+// RPG Maker Backup Utility.
+// Version: 1.1
+////////////////////////////////
+// Changelog:
+// - Hooked into RPGMaker MV's internal methods for grabbing storage Keys
+// - Implemented compression and decompression functions
+// - Implemented compression setting flag.
+// - Implemented local file check.
+////////////////////////////////
+
+// Settings.
+let decompressionEnabled = true;
+
 let saveInit = false;
 let canImport = false;
 let recoveryData = null;
 
 const gamePrefix = 'RPG';
 
+
+// Runs decompression on data if it's enabled.
+function decompress(source) {
+    if(decompressionEnabled) {
+        return LZString.decompressFromBase64(source);
+    }
+    
+    return(source);
+}
+
+// Runs compression on data if it's in JSON format.
+function compress(source) {
+    try {
+        JSON.parse(source)
+        return LZString.compressToBase64(source);
+    } 
+    catch (e) {
+        return source;
+    }
+}
+
+// Finds and retrieves all available save data, up to 100 save files.
+function getSaveData() {
+    let data = {
+        global: null,
+        config: null,
+        saves: []
+    };
+    
+    for (let i = -1; i < 100; i++) {
+        let ID = StorageManager.webStorageKey(i);
+    
+        if(i === -1 && localStorage.getItem(ID) !== null) {
+            data.config = {
+                name: ID, 
+                data: decompress(localStorage.getItem(ID))
+            }
+        }
+        
+        if(i === 0 && localStorage.getItem(ID) !== null) {
+            data.global = {
+                name: ID, 
+                data: decompress(localStorage.getItem(ID))
+            }
+        }
+        
+        if(i > 0 && localStorage.getItem(ID) !== null) {
+            data.saves.push({
+                name: ID, 
+                data: decompress(localStorage.getItem(ID))
+            });
+        }
+    }
+    
+    data.saveCount = data.saves.length;
+    
+    return data;
+}
+
 function initSaveManager(){
 	document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
+	
+	// Present error if running from a file.
+	if(window.location.protocol !== "http:" && window.location.protocol !== "https:") {
+	    Alert(`ERROR: This game appears to be running from a local file.
+\nDue to the nature of RPG Maker MV games, you'll need to host this from a web server such as Apache or NGINX.
+        
+\n\nFor Ubuntu users, run this command: "sudo apt install apache2" to install a web server.
+
+For Fedora users, run this command: "sudo dnf install httpd" instead to install web server.
+
+Once a web server is installed, copy the contents of the directory containing this index.html file to "/var/www/html/", You will likely need to access this directory as root.
+
+For windows users, I recommend trying XAMMP or an equivalent software. I have not tried this under Windows so I can't help any further.`);
+	}
 }
 
 function handleFileSelect(event){
@@ -24,6 +111,8 @@ function handleFileLoad(event){
 	}
 }
 
+
+// Exports saves. Assumes a standard save file naming convention.
 function ExportSaves(filename) {
 	ExportData = {};
 
